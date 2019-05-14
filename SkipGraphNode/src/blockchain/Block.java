34f4cabeba42e2d2707d81450ed2cd2e8346bfb0
shamdan17 <shamdan17@ku.edu.tr>
2019-05-14 12:47:00 +0300

    Initial commit

---
+++ SkipGraphNode/src/blockchain/Block.java
@@ -0,0 +1,13 @@
+package blockchain;
+
+import java.util.Set;
+
+public class Block {
+	private static final String prev;
+	private static final String owner;
+	private static final Set<Transaction> S;
+	private static final String search_proof;
+	private static final String h;
+	private static final String sigma;
+	
+}
