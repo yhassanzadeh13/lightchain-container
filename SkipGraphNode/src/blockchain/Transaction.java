506ec16ba0bac0a1e1c111fae133b7731102d592
shamdan17 <shamdan17@ku.edu.tr>
2019-05-28 11:56:02 +0300

    incomplete transaction class added

--- SkipGraphNode/src/blockchain/Transaction.java
+++ SkipGraphNode/src/blockchain/Transaction.java
@@ -1,15 +1,16 @@
-package blockchain;
-
-public class Transaction {
-	private static final String prev;
-	private static final String owner;
-	private static final String cont;
-	private static final String search_proof;//Type?
-	private static final String h;
-	private static final String sigma;
-	public Transaction() {
-		
-	}
-
-		
-}
+//package blockchain;
+//
+//public class Transaction {
+//	private static final String prev;
+//	private static final String owner;
+//	private static final String cont;
+//	private static final String search_proof;//Type?
+//	private static final String h;
+//	private static final String sigma;
+//	
+//	public Transaction() {
+//		
+//	}
+//
+//		
+//}
