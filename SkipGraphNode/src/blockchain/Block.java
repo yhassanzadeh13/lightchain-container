5ef0a5fcda07e5161133329885597680a8cf2fb5
shamdan17 <shamdan17@ku.edu.tr>
2019-05-28 13:02:25 +0300

    Added constructors for block and transactions

--- SkipGraphNode/src/blockchain/Block.java
+++ SkipGraphNode/src/blockchain/Block.java
@@ -1,13 +1,25 @@
 package blockchain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Block {
-	private static final String prev;
-	private static final String owner;
-	private static final Set<Transaction> S;
-	private static final String search_proof;
-	private static final String h;
-	private static final String sigma;
+	private final String prev;
+	private final String owner;
+	private final Set<Transaction> S;
+	private final String search_proof;
+	private final String h;
+	private final String sigma;
+	
+	public Block(String prev, String owner, String search_proof, String h, String sigma) {
+		super();
+		this.prev = prev;
+		this.owner = owner;
+		S = new HashSet<Transaction>();
+		this.search_proof = search_proof;
+		this.h = h;
+		this.sigma = sigma;
+	}
+	
 	
 }
--- SkipGraphNode/src/blockchain/Transaction.java
+++ SkipGraphNode/src/blockchain/Transaction.java
@@ -1,16 +1,23 @@
-//package blockchain;
-//
-//public class Transaction {
-//	private static final String prev;
-//	private static final String owner;
-//	private static final String cont;
-//	private static final String search_proof;//Type?
-//	private static final String h;
-//	private static final String sigma;
-//	
-//	public Transaction() {
-//		
-//	}
-//
-//		
-//}
+package blockchain;
+
+public class Transaction {
+	private final String prev;
+	private final String owner;
+	private final String cont;
+	private final String search_proof;//Type?
+	private final String h;
+	private final String sigma;
+	
+	
+	public Transaction(String prev, String owner, String cont, String search_proof, String h, String sigma) {
+		super();
+		this.prev = prev;
+		this.owner = owner;
+		this.cont = cont;
+		this.search_proof = search_proof;
+		this.h = h;
+		this.sigma = sigma;
+	}
+
+		
+}
