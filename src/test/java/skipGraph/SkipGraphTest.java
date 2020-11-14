b817512ed1182c3949a93553af8f198f8994920d
Sarieh <ssrdar18@ku.edu.tr>
2020-11-14 18:34:06 +0300

    create Localnet test "2 Nodes Concurrent Insertion"

--- src/test/java/skipGraph/SkipGraphTest.java
+++ src/test/java/skipGraph/SkipGraphTest.java
@@ -1,7 +1,6 @@
 package skipGraph;
 
 import blockchain.Block;
-import blockchain.Transaction;
 import localnet.LocalSkipGraph;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,7 +32,7 @@ public class SkipGraphTest {
         config2 = new NodeConfig(maxLevels, port++, numID3, nameID3);
     }
 
-    @Test
+    //    @Test
     void oneNodeSequentialBlocksInsertion() {
         try {
             SkipNode node = new SkipNode(initialConfig, Const.DUMMY_INTRODUCER, true);
@@ -55,12 +54,12 @@ public class SkipGraphTest {
 
             //insert 100 blocks
             Block latestBlock = genesis;
-            for(int i = 0; i< 100; i++){
+            for (int i = 0; i < 100; i++) {
 
                 NodeInfo tmpCorrect = localGraph.searchByNumID(latestBlock.getNumID());
                 NodeInfo tmpTest = node.searchByNumID(latestBlock.getNumID());
 
-                assert(tmpCorrect instanceof Block && tmpTest instanceof Block && tmpTest.getNameID().equals(tmpCorrect.getNameID()));
+                assert (tmpCorrect instanceof Block && tmpTest instanceof Block && tmpTest.getNameID().equals(tmpCorrect.getNameID()));
                 Block blk = (Block) tmpCorrect;
                 Block newBlk = new Block(blk.getHash(), node.getNumID(), node.getAddress(), new ArrayList<>(), blk.getIndex() + 1, maxLevels);
                 localGraph.insertNode(newBlk);
@@ -71,7 +70,8 @@ public class SkipGraphTest {
             e.printStackTrace();
         }
     }
-    //@Test
+
+    //    @Test
     void twoNodesSequentialBlocksInsertion() {
         try {
             SkipNode node1 = new SkipNode(initialConfig, Const.DUMMY_INTRODUCER, true);
@@ -97,11 +97,11 @@ public class SkipGraphTest {
 
             //insert 200 blocks
             Block latestBlock = genesis;
-            for(int i = 0; i< 100; i++){
+            for (int i = 0; i < 100; i++) {
                 //let node1 insert a block
                 NodeInfo tmpCorrect = localGraph.searchByNumID(latestBlock.getNumID());
                 NodeInfo tmpTest = node1.searchByNumID(latestBlock.getNumID());
-                assert(tmpCorrect instanceof Block && tmpTest instanceof Block && tmpCorrect.getNameID().equals(tmpTest.getNameID()));
+                assert (tmpCorrect instanceof Block && tmpTest instanceof Block && tmpCorrect.getNameID().equals(tmpTest.getNameID()));
                 Block blk = (Block) tmpCorrect;
                 Block newBlk = new Block(blk.getHash(), node1.getNumID(), node1.getAddress(), new ArrayList<>(), blk.getIndex() + 1, maxLevels);
                 localGraph.insertNode(newBlk);
@@ -111,7 +111,7 @@ public class SkipGraphTest {
 
                 tmpCorrect = localGraph.searchByNumID(latestBlock.getNumID());
                 tmpTest = node2.searchByNumID(latestBlock.getNumID());
-                assert(tmpCorrect instanceof Block && tmpTest instanceof Block && tmpCorrect.getNameID().equals(tmpTest.getNameID()));
+                assert (tmpCorrect instanceof Block && tmpTest instanceof Block && tmpCorrect.getNameID().equals(tmpTest.getNameID()));
                 blk = (Block) tmpCorrect;
                 newBlk = new Block(blk.getHash(), node2.getNumID(), node2.getAddress(), new ArrayList<>(), blk.getIndex() + 1, maxLevels);
                 localGraph.insertNode(newBlk);
@@ -122,53 +122,73 @@ public class SkipGraphTest {
         }
     }
 
+    @Test
     void twoNodesConcurrentBlocksInsertion() {
+        try {
+            SkipNode node1 = new SkipNode(initialConfig, Const.DUMMY_INTRODUCER, true);
+            SkipNode node2 = new SkipNode(config1, node1.getAddress(), false);
+            List<NodeInfo> list = new ArrayList<>();
 
-//        try {
-//            SkipNode node1 = new SkipNode(initialConfig, Const.DUMMY_INTRODUCER, true);
-//            SkipNode node2 = new SkipNode(config1, node1.getAddress(), false);
-//
-//            List<NodeInfo> list = new ArrayList<NodeInfo>();
-//            list.add(node1.peerNode);
-//            list.add(node2.peerNode);
-//            LocalSkipGraph localGraph = new LocalSkipGraph(list, maxLevels);
-//
-//            //create and insert genesis
-//            StringBuilder st = new StringBuilder();
-//            for (int i = 0; i < maxLevels; i++)
-//                st.append("0");
-//            String prev = st.toString();
-//            int index = 0;
-//            Block genesis = new Block(prev, node1.getNumID(), node1.getAddress(), index, maxLevels);
-//            localGraph.insertNode(genesis);
-//
-//            //insert 200 blocks
-//            Block latestBlock = genesis;
-//            for(int i = 0; i< 100; i++){
-//                //let node1 insert a block
-//                NodeInfo tmp = localGraph.searchByNumID(latestBlock.getNameID());
-//                assert(tmp instanceof Block && tmp.getNameID().equals(latestBlock.getNameID()));
-//                Block blk = (Block) tmp;
-//                Block newBlk = new Block(blk.getHash(), node1.getNumID(), node1.getAddress(), new ArrayList<>(), blk.getIndex() + 1, maxLevels);
-//                localGraph.insertNode(newBlk);
-//                latestBlock = newBlk;
-//
-//                //let node2 insert a block
-//                tmp = localGraph.searchByNumID(latestBlock.getNameID());
-//                assert(tmp instanceof Block && tmp.getNameID().equals(latestBlock.getNameID()));
-//                blk = (Block) tmp;
-//                newBlk = new Block(blk.getHash(), node2.getNumID(), node2.getAddress(), new ArrayList<>(), blk.getIndex() + 1, maxLevels);
-//                localGraph.insertNode(newBlk);
-//                latestBlock = newBlk;
-//            }
-//        } catch (RemoteException e) {
-//            e.printStackTrace();
-//        }
+            list.add(node1.getPeer());
+            list.add(node2.getPeer());
+
+            LocalSkipGraph localGraph = new LocalSkipGraph(list, maxLevels);
+
+            //create and insert genesis
+            StringBuilder st = new StringBuilder();
+            for (int i = 0; i < maxLevels; i++)
+                st.append("0");
+
+            String prev = st.toString();
+            int index = 0;
+            Block genesis = new Block(prev, node1.getNumID(), node1.getAddress(), index, maxLevels);
+            localGraph.insertNode(genesis);
+
+            node1.insertNode(genesis);
+
+
+            final Block[] latestBlock = {genesis};
+            Thread t1 = new Thread(() -> {
+                for (int i = 0; i < 100; i++) {
+                    //let node1 insert a block
+                    NodeInfo tmpCorrect = localGraph.searchByNumID(latestBlock[0].getNumID());
+                    NodeInfo tmpTest = node1.searchByNumID(latestBlock[0].getNumID());
+                    assert (tmpCorrect instanceof Block && tmpTest instanceof Block && tmpCorrect.getNameID().equals(tmpTest.getNameID()));
+                    Block blk = (Block) tmpCorrect;
+                    Block newBlk = new Block(blk.getHash(), node1.getNumID(), node1.getAddress(), new ArrayList<>(), blk.getIndex() + 1, maxLevels);
+                    localGraph.insertNode(newBlk);
+                    node1.insertNode(newBlk);
+                    latestBlock[0] = newBlk;
+
+                }
+            }) {
+            };
+
+            Thread t2 = new Thread(() -> {
+                for (int i = 0; i < 100; i++) {
+                    //let node2 insert a block
+                    NodeInfo tmpCorrect = localGraph.searchByNumID(latestBlock[0].getNumID());
+                    NodeInfo tmpTest = node2.searchByNumID(latestBlock[0].getNumID());
+                    assert (tmpCorrect instanceof Block && tmpTest instanceof Block && tmpCorrect.getNameID().equals(tmpTest.getNameID()));
+                    Block blk = (Block) tmpCorrect;
+                    Block newBlk = new Block(blk.getHash(), node2.getNumID(), node2.getAddress(), new ArrayList<>(), blk.getIndex() + 1, maxLevels);
+                    localGraph.insertNode(newBlk);
+                    node2.insertNode(newBlk);
+                }
+            }) {
+            };
+
+            t1.start();
+            t2.start();
+
+            while(t1.isAlive() || t2.isAlive()){
+
+            }
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
-
-
-
-
 
 
 }
