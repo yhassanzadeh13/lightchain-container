c14df909a543a5a6de3e9775d17ae9ec79f65b5b
Bulut Bulgu <bbulgudacity@gmail.com>
2021-05-31 22:02:02 +0300

    Add javadoc to underlay methods

--- src/main/java/underlay/Underlay.java
+++ src/main/java/underlay/Underlay.java
@@ -23,11 +23,35 @@ public abstract class Underlay {
     this.lightChainNode = lightChainNode;
   }
 
+  /**
+   * This method is used to send a request to a remote server specified by the address
+   * The target server then uses the answer method below to call the underlying LightChainNode
+   * or the SkipNode using the request and its specific data
+   * @param req             The request which will be invoked on the remote server
+   * @param targetAddress   The address used to specify the target remote server
+   * @return                The response returned by the remote server
+   * @throws RemoteException
+   * @throws FileNotFoundException
+   */
   public abstract GenericResponse sendMessage(GenericRequest req, String targetAddress)
       throws RemoteException, FileNotFoundException;
 
+
+  /**
+   * This is the method that is invoked when this underlay instance is sent a message
+   * by another underlay. The request is transmitted to the underlying LightChainNode
+   * or the SkipNode.
+   * @param req     The request
+   * @return        The response emitted
+   * @throws RemoteException
+   * @throws FileNotFoundException
+   */
   public abstract GenericResponse answer(GenericRequest req)
       throws RemoteException, FileNotFoundException;
 
+  /**
+   * Terminates the underlay.
+   * @return true iff the termination was successful.
+   */
   public abstract boolean terminate();
 }
