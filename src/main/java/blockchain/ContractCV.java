b53412d98425c15da6293bcb528be35927378510
yhassanzadeh13@ku.edu.tr <yhassanzadeh13@ku.edu.tr>
2021-06-10 08:09:41 -0700

    updates correctness verifier for lightchain

--- src/main/java/blockchain/ContractCV.java
+++ src/main/java/blockchain/ContractCV.java
@@ -3,9 +3,7 @@ package blockchain;
 import evm.Contract;
 import evm.ContractTransaction;
 import skipGraph.NodeInfo;
-import underlay.InterfaceType;
 import underlay.Underlay;
-import underlay.rmi.RMIUnderlay;
 import underlay.requests.lightchain.GetTokenRequest;
 import underlay.responses.IntegerResponse;
 
@@ -18,36 +16,38 @@ import static underlay.responses.IntegerResponse.IntegerResponseOf;
  * TransctSol()
  */
 class ContractCV extends CorrectnessVerifier {
-  Underlay underlay;
-  Contract ct = new Contract();
-  ContractTransaction tesq = new ContractTransaction();
+    Underlay underlay;
+    Contract ct = new Contract();
+    ContractTransaction tesq = new ContractTransaction();
 
-  public ContractCV(LightChainNode owner) throws RemoteException {
-    super(owner);
-    this.underlay = owner.getUnderlay();
-  }
-
-  /**
-   * Checks correctness of transaction by passing values to smart contracts. Returns true if the
-   * conditions are met.
-   *
-   * @param t transaction whose correctness is to be verified
-   * @return true if transaction is correct, or false if not
-   */
-  @Override
-  public boolean isCorrect(Transaction t) {
-    try {
-      NodeInfo ndowner = owner.searchByNumID(t.getOwner());
-      IntegerResponse response = IntegerResponseOf(underlay.sendMessage(
-              new GetTokenRequest(),
-              ndowner.getAddress()));
-      int token1 = response.result;
-      tesq.setup();
-      boolean value = tesq.TransctSol(token1, ct.contractName1, ct.functname1);
-      return value;
-    } catch (Exception e) {
-      e.printStackTrace();
-      return false;
+    public ContractCV(LightChainNode owner) throws RemoteException {
+        super(owner);
+        this.underlay = owner.getUnderlay();
     }
-  }
+
+    /**
+     * Checks correctness of transaction by passing values to smart contracts. Returns true if the
+     * conditions are met.
+     *
+     * @param t transaction whose correctness is to be verified
+     * @return true if transaction is correct, or false if not
+     */
+    @Override
+    public boolean isCorrect(Transaction t) {
+        try {
+
+            NodeInfo txOwner = owner.searchByNumID(t.getOwner());
+            IntegerResponse response = IntegerResponseOf(underlay.sendMessage(
+                    new GetTokenRequest(),
+                    txOwner.getAddress()));
+            int token1 = response.result;
+            tesq.setup();
+            return tesq.TransctSol(token1, ct.contractName1, ct.functname1);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
